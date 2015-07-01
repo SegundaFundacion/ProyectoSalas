7968910db6033c4617d62f82b9263d4f8c17f436
Xio230 <Xio.230@gmail.com>
2015-07-01 02:08:52 -0300

    menuadmin

--- resources/views/menuadministrador/index.blade.php
+++ resources/views/menuadministrador/index.blade.php
@@ -1,4 +1,28 @@
 @extends('app')
 @section('content')
-
+<br><br><br>
+<section id="main-content">
+          <section class="wrapper">
+              <div class="row mt">
+                  <div class="col-md-12">
+                      <div class="content-panel">
+                      <center>
+                          <a href="/menuadmin"  class="btn btn-lg btn-primary">Administrador</a>
+                          <a href="/home"  class="btn btn-lg btn-success">Encargado</a>
+                          <a href="/campus"  class="btn btn-lg btn-primary">Docente</a>
+                          <a href="/campus"  class="btn btn-lg btn-success">Estudiante</a>
+                          <a href="/menuadmin"  class="btn btn-lg btn-primary">Administrador</a>
+                          <a href="/home"  class="btn btn-lg btn-success">Encargado</a>
+                          <a href="/campus"  class="btn btn-lg btn-primary">Docente</a>
+                          <a href="/campus"  class="btn btn-lg btn-success">Estudiante</a>
+                          <a href="/campus"  class="btn btn-lg btn-primary">Estudiante</a>
+                      </center>
+        
+        
+     
+                      </div><!-- /content-panel -->
+                  </div><!-- /col-md-12 -->
+              </div><!-- /row -->
+              </section>
+              </section>
 @endsection
