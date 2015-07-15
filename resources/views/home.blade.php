64d4dda1fe31ac574edeb9e92748dfb68820189d
Xio230 <Xio.230@gmail.com>
2015-07-15 01:26:18 -0300

    Quitado mensaje de logeo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,22 +5,7 @@
   <br>
    <br>
    
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
 
-				<div class="panel-body">
-					Te has logueado exitosamente
-
-				</div>
-
-			</div>
-
-		</div>
-	</div>
-</div>
 <br>
    <br>
 <center>
